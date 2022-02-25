a19cd20043613e230716d611d84abadf2be0cca5
stevencueto <steven.cueto1305@gmail.com>
2022-02-25 04:00:41 -0500

    nav bar and toggle bar made

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 console.log('It Works')
 
-$('h1').css('color', 'blue');
+document.addEventListener('click', e=> {
+    const toggleMenu = e.target.closest('.toggle-button');
+    const navMenuClick = document.querySelector('.nav-elements')
+
+    if(toggleMenu){
+        navMenuClick.classList.toggle('active')
+        return
+    }
+    let currentMenu
+    if(currentMenu){
+        currentMenu = document.closest('.nav-elements');
+        navMenuClick.classList.toggle('active')
+    }
+
+    if(currentMenu){
+        return
+    }else{
+        navMenuClick.classList.remove('active')
+    }
+    
+})
+
+
